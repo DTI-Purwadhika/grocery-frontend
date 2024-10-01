ec288634b8f2789aa586c9724dd21b675cff1c9f
Christofer Bryan <christoferbryan45@gmail.com>
2024-10-01 23:21:43 +0700

    feat(auth): added user name, role, and profile picture to dashboard header and fixed middleware

--- src/components/elements/User/index.tsx
+++ src/components/elements/User/index.tsx
@@ -3,37 +3,48 @@ import { Card, CardBody } from "@nextui-org/card";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/dropdown";
 import { User } from "@nextui-org/user";
 import { User2 } from "lucide-react";
+import { useProfile } from "@/hooks/useProfile";
 
 import { ThemeButton, Notification } from "..";
 
-const UserCard = () => (
-  <div className="flex flex-row gap-4">
-    <Card className="hidden md:flex">
-      <CardBody className="flex flex-row gap-2">
-        <ThemeButton />
-        <Notification />
-      </CardBody>
-    </Card>
-    <Dropdown>
-      <DropdownTrigger>
-        <Card className="cursor-pointer">
-          <CardBody>
-            <User
-              className="hidden md:flex md:flex-nowrap w-fit"
-              description="Admin Store"
-              name="John Doe"
-            />
-            <User2 className="md:hidden" />
+const UserCard = () => {
+  const { userProfile } = useProfile();
+
+  return (
+    <>
+      <div className="flex flex-row gap-4">
+        <Card className="hidden md:flex">
+          <CardBody className="flex flex-row gap-2">
+            <ThemeButton />
+            <Notification />
           </CardBody>
         </Card>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Static Actions">
-        <DropdownItem key="dashboard">Dashboard</DropdownItem>
-        <DropdownItem key="profile">Profile</DropdownItem>
-        <DropdownItem key="logout">Logout</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-  </div>
-);
+        <Dropdown>
+          <DropdownTrigger>
+            <Card className="cursor-pointer">
+              <CardBody>
+                <User
+                  className="hidden md:flex md:flex-nowrap w-fit"
+                  description={userProfile?.role}
+                  name={userProfile?.name}
+                  avatarProps={{
+                    src: `
+                    ${userProfile?.profilePicture}`,
+                  }}
+                />
+                <User2 className="md:hidden" />
+              </CardBody>
+            </Card>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions">
+            <DropdownItem key="dashboard">Dashboard</DropdownItem>
+            <DropdownItem key="profile">Profile</DropdownItem>
+            <DropdownItem key="logout">Logout</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    </>
+  );
+};
 
 export default UserCard;
--- src/middleware.ts
+++ src/middleware.ts
@@ -23,15 +23,7 @@ export default auth( async (request : NextRequest) => {
   const path = reqUrl.pathname
 
     const session = await auth();
-    const publicRoutes = ['/', '/login', '/register', '/catalog','/reset-password', '/reset-password-request', '/set-password',]
-    const adminRoutes = [
-        '/dashboard'
-    ]
-    const customerRoutes = [                                                                                                                                                                                     
-        '/my-cart',
-        '/my-profile',
-        '/my-favorite',
-      ]
+    const publicRoutes = ['/', '/login', '/register', '/catalog', '/catalog/:id','/reset-password', '/reset-password-request', '/set-password',];
 
     if (publicRoutes.includes(path)) {
         return NextResponse.next()
@@ -45,15 +37,15 @@ export default auth( async (request : NextRequest) => {
     const userRole = session.user?.role;
         
     if (userRole === "SUPER" || userRole === "ADMIN" ) {
-        if (!adminRoutes.includes(path)) {
+        if (!path.startsWith("/dashboard")) {
             return NextResponse.redirect(new URL("/dashboard", request.url));
             }
     }
 
     if(userRole === "CUSTOMER"){
-        if(!customerRoutes.includes(path)){
-            return NextResponse.redirect(new URL("/", request.url));
-        }
+        if(!path.match(/^\/my-/)){
+          return NextResponse.redirect(new URL("/", request.url));
+      }
     }   
 
     return NextResponse.next();
