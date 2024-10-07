fffbb74dc3ec36767b3afd4d10143c413cb0c1ba
Christofer Bryan <christoferbryan45@gmail.com>
2024-10-07 18:14:32 +0700

    feat(homepage): added product catalog to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import { FooterHome } from "@/components/layout/footer-homepage";
 import { Hero } from "@/components/layout/hero";
 import { NavBar } from "@/components/layout/navbar";
+import { ProductSection } from "@/components/layout/product-section";
 
 const Home = () => (
-  <main className="pt-24">
+  <main className="relative pt-36 lg:pt-24">
     <NavBar />
     <Hero />
+    <ProductSection />
     <FooterHome />
   </main>
 );
