658b4c3b00a9367bf282d9eac6dd59d46d60b3c2
Christofer Bryan <christoferbryan45@gmail.com>
2024-10-10 18:19:49 +0700

    feat(address): displayed user addresses on checkout page and added create new address feature

--- src/components/data/CheckoutList/Card/address.tsx
+++ src/components/data/CheckoutList/Card/address.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { AddressDataResponse, useAddress } from "@/hooks/useAddress";
+import { getPrimaryAddress, PrimaryAddress } from "@/hooks/PrimaryAddress";
 import { Button } from "@nextui-org/button";
 import { Card, CardBody } from "@nextui-org/card";
 import {
@@ -10,30 +12,111 @@ import {
   useDisclosure,
 } from "@nextui-org/modal";
 import { Pin, ChevronRight } from "lucide-react";
+import { useEffect, useState } from "react";
+import { getCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { FaTrash } from "react-icons/fa";
 
 const Delivery = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const router = useRouter();
+  const [deleteAddressModal, setDeleteAddressModal] = useState<boolean>(false);
+  const primaryAddressData: PrimaryAddress | undefined = getPrimaryAddress();
+  const addresses: AddressDataResponse[] | undefined = useAddress();
+  const [primaryAddressId, setPrimaryAddressId] = useState<number>();
+  const [primaryAddressName, setPrimaryAddressName] = useState<string>();
+  const [primaryAddressCity, setPrimaryAddressCity] = useState<string>();
+  const cookieValue = getCookie("Sid");
+
+  useEffect(() => {
+    setPrimaryAddressId(primaryAddressData?.id);
+    setPrimaryAddressName(primaryAddressData?.addressName);
+    setPrimaryAddressCity(primaryAddressData?.city);
+  }, [primaryAddressData]);
+
+  const closeDeleteAddressModal = () => {
+    setDeleteAddressModal(false);
+  };
+
+  const choosePrimaryAddress = async (id: number) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/addresses/change-primary-address/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            // @ts-ignore
+            Authorization: `Bearer ${cookieValue}`,
+          },
+          credentials: "include",
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to change primary address");
+      }
+
+      const data = await response.json();
+
+      setPrimaryAddressId(id);
+      setPrimaryAddressName(data.data.addressName);
+      setPrimaryAddressCity(data.data.city);
+      onClose();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const addressContent = (
     <div className="flex flex-col gap-2 ">
-      <Card shadow="sm">
-        <CardBody className="flex flex-row justify-between px-5 py-4 items-end">
-          <div className="flex flex-col gap-2">
-            <h3 className="font-semibold text-md">Primary Address</h3>
-            <div className="flex flex-row items-start gap-2 text-xs">
-              <Pin className="text-primary text-sm" size={16} />
-              <p>
-                jalan. huha huha
-                <br />
-                (816) 231-0053
-              </p>
-            </div>
-          </div>
-          <Button color="primary" size="sm" onPress={onClose}>
-            Choose
-          </Button>
-        </CardBody>
-      </Card>
+      {addresses.length > 0 ? (
+        addresses.map((address) => (
+          <>
+            <Card key={address.id} shadow="sm">
+              <CardBody className="flex flex-col justify-between px-5 py-4 items-end">
+                <div className="flex flex-col gap-2">
+                  {/* <h3 className="font-semibold text-md">Primary Address</h3> */}
+                  <div className="flex flex-row items-start gap-2 text-xs">
+                    {primaryAddressId === address.id && (
+                      <Pin className="text-primary text-sm w-6 h-6" />
+                    )}
+                    <p>
+                      <span className="font-bold">Address Line:</span> {address.addressName}
+                      <br />
+                      <br />
+                      <span className="font-bold">City:</span> {address.city}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-4">
+                  {primaryAddressId !== address.id && (
+                    <>
+                      <Button
+                        color="primary"
+                        size="sm"
+                        onPress={() => choosePrimaryAddress(address.id)}
+                      >
+                        Choose
+                      </Button>
+                    </>
+                  )}
+                  <Button size="sm" color="danger" isIconOnly>
+                    <FaTrash />
+                  </Button>
+                </div>
+              </CardBody>
+            </Card>
+          </>
+        ))
+      ) : (
+        <>
+          <p className="font-semibold text-sm">
+            You have no address yet, please add a new address.
+          </p>
+        </>
+      )}
     </div>
   );
 
@@ -49,9 +132,10 @@ const Delivery = () => {
             <div className="flex flex-row items-start gap-2 text-xs">
               <Pin className="text-primary text-sm" size={16} />
               <p>
-                jalan. huha huha
+                {primaryAddressName}
                 <br />
-                (816) 231-0053
+                <br />
+                {primaryAddressCity}
               </p>
             </div>
           </div>
@@ -65,9 +149,32 @@ const Delivery = () => {
               <ModalHeader className="flex flex-col gap-1">Choose Delivery Address</ModalHeader>
               <ModalBody>{addressContent}</ModalBody>
               <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
+                <Link href="/my-cart/checkout/create-address">
+                  <Button color="success" variant="light">
+                    Add New Address
+                  </Button>
+                </Link>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <Modal backdrop={"blur"} isOpen={deleteAddressModal} onClose={closeDeleteAddressModal}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Delete Address</ModalHeader>
+              <ModalBody>
+                <p className="font-semibold">
+                  Are you sure you want to delete this address? This action can't be undone!
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Link href="/my-cart/checkout/create-address">
+                  <Button color="success" variant="light">
+                    Add New Address
+                  </Button>
+                </Link>
               </ModalFooter>
             </>
           )}
