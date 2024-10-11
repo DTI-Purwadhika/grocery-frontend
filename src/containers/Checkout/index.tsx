e619a68b9b9ec2b6e857390801e6994793c531f2
Christofer Bryan <christoferbryan45@gmail.com>
2024-10-11 18:52:25 +0700

    fix(homepage): fixed footer on homepage

--- src/containers/Checkout/index.tsx
+++ src/containers/Checkout/index.tsx
@@ -2,7 +2,7 @@ import { Datacheckout, Total } from "@/components/data";
 
 const Checkout = () => {
   return (
-    <div className="grid grid-rows-[1fr_auto] h-full">
+    <div className="grid grid-rows-[1fr_auto] h-full gap-4 lg:gap-2">
       <Datacheckout />
       <Total source="checkout" />
     </div>
