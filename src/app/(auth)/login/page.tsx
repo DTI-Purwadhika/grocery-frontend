db3de4021abb40c1836d20ba6c89938c3da5ce94
Christofer Bryan <christoferbryan45@gmail.com>
2024-10-13 21:45:05 +0700

    feat(auth): changed middleware configurations and added suspense fallback to user authentication pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,11 @@
 import React, { Suspense } from "react";
 import { LoginForm } from "./components/Login";
+import { SuspenseFallback } from "@/components/elements/SuspenseFallback";
 
 const login: React.FC = () => {
   return (
     <>
-      <Suspense>
+      <Suspense fallback={<SuspenseFallback />}>
         <main className="bg-green-500">
           <LoginForm />
         </main>
--- src/app/set-password/page.tsx
+++ src/app/set-password/page.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
 import { SetPasswordForm } from "./components/SetPassword";
+import { SuspenseFallback } from "@/components/elements/SuspenseFallback";
 // import { SetPasswordForm } from "./components/SetPassword/index2";
 
 const register = () => {
   return (
     <>
-      <Suspense>
+      <Suspense fallback={<SuspenseFallback />}>
         <main>
           <SetPasswordForm />
         </main>
