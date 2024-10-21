92d464cbec0945edd23221469b6d83747c61925d
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-10-21 13:05:22 +0700

    fix: :bug: make the image centered

--- src/containers/Product/index.tsx
+++ src/containers/Product/index.tsx
@@ -110,7 +110,7 @@ const ProductDetail = ({ id }: ProductDetailType) => {
           <Image
             isZoomed
             alt={collectData?.images[0]?.altText || collectData?.name}
-            className="w-full aspect-video object-contain"
+            className="w-full aspect-video object-contain mx-auto"
             fallbackSrc="https://via.placeholder.com/500x500"
             src={collectData?.images[0]?.url}
             width={500}
